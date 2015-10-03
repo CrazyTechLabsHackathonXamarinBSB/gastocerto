3942df9510d8fabde9530f088e91b2dfdff37480
ivaldodeoliveira@live.com <ivaldodeoliveira@live.com>
2015-10-03 18:02:30 -0300

    ..

--- GastoCerto/GastoCerto.Core/Modelo/Despesa.cs
+++ GastoCerto/GastoCerto.Core/Modelo/Despesa.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite.Net.Attributes;
+using System;
 
 namespace GastoCerto.Core.Modelo
 {
     public class Despesa
     {
+        [PrimaryKey, AutoIncrement]
         public long id { get; set; }
         public DateTime data { get; set; }
         public string justificativa { get; set; }
-
         public decimal valor { get; set; } 
     }
 }
--- GastoCerto/GastoCerto.Core/Repositorio/DespesaRepositorio.cs
+++ GastoCerto/GastoCerto.Core/Repositorio/DespesaRepositorio.cs
@@ -1,13 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cirrious.CrossCore;
+using GastoCerto.Core.Modelo;
+using MvvmCross.Plugins.Sqlite;
+using SQLite.Net.Async;
 
 namespace GastoCerto.Core.Repositorio
 {
     class DespesaRepositorio
     {
+        private SQLiteAsyncConnection _connection;
+
+        public async void Init()
+        {
+            var connectionFactory = Mvx.Resolve<IMvxSqliteConnectionFactory>();
+            _connection = connectionFactory.GetAsyncConnection("GastoCertoBD");
+
+            await _connection.CreateTableAsync<Despesa>();
+        }
+
+        public async void Inserir(Despesa despesa)
+        {
+            await _connection.InsertAsync(despesa);
+           // _connection.Table<Despesa>()
+        }
+
+        public async void Excluir(Despesa despesa)
+        {
+            await _connection.DeleteAsync(despesa);
+        }
+
+        //public async ObserverCollection<Despesa> Consultar()
+        //{
+        //    await _connection.InsertAsync(despesa);
+        //    // _connection.Table<Despesa>()
+        //}
+
+
 
     }
 }
