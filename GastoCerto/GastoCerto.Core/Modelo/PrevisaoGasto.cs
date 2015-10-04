fb52893ac37b21f353329d8f84a58b70e6a45968
ivaldodeoliveira@live.com <ivaldodeoliveira@live.com>
2015-10-04 04:59:21 -0300

    ..

--- GastoCerto/GastoCerto.Core/Modelo/PrevisaoGasto.cs
+++ GastoCerto/GastoCerto.Core/Modelo/PrevisaoGasto.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
-
+ 
 namespace GastoCerto.Core.Modelo
 {
     public class PrevisaoGasto : MvxNotifyPropertyChanged
