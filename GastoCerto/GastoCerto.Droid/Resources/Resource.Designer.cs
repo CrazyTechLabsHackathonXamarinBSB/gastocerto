df592af5f941671ddcfb4579be047b2df2491d37
ivaldodeoliveira@live.com <ivaldodeoliveira@live.com>
2015-10-04 01:18:35 -0300

    ..

--- GastoCerto/GastoCerto.Droid/Resources/Resource.Designer.cs
+++ GastoCerto/GastoCerto.Droid/Resources/Resource.Designer.cs
@@ -139,23 +139,26 @@ namespace GastoCerto.Droid
 			// aapt resource value: 0x7f050001
 			public const int MvxBindingTagUnique = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int MyButton = 2131034114;
+			// aapt resource value: 0x7f050005
+			public const int MyButton = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnBuscar = 2131034119;
 			
 			// aapt resource value: 0x7f050006
-			public const int btnBuscar = 2131034118;
-			
-			// aapt resource value: 0x7f050005
-			public const int btnIncluir = 2131034117;
+			public const int btnIncluir = 2131034118;
 			
 			// aapt resource value: 0x7f050004
 			public const int btnSalvar = 2131034116;
 			
-			// aapt resource value: 0x7f050007
-			public const int btnVoltar = 2131034119;
+			// aapt resource value: 0x7f050008
+			public const int btnVoltar = 2131034120;
 			
 			// aapt resource value: 0x7f050003
 			public const int editText1 = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText2 = 2131034114;
 			
 			static Id()
 			{
