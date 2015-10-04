1e339c169f045cfdaa11fdd0cc7d28031b6d48ef
joreld <vasconcelosjorel@gmail.com>
2015-10-04 03:12:36 -0300

    Atualização Adocopmar Despesas

--- GastoCerto/GastoCerto.Core/App.cs
+++ GastoCerto/GastoCerto.Core/App.cs
@@ -13,13 +13,13 @@ namespace GastoCerto.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 
-            var previsaoGastoRepositorio = new PrevisaoGastoRepositorio();
-            previsaoGastoRepositorio.Init();
-            Mvx.RegisterSingleton(previsaoGastoRepositorio);
+            var previsaogastorepositorio = new previsaogastorepositorio();
+            previsaogastorepositorio.init();
+            mvx.registersingleton(previsaogastorepositorio);
 
-            var despesaRepositorio = new DespesaRepositorio();
-            despesaRepositorio.Init();
-            Mvx.RegisterSingleton(despesaRepositorio);
+            var despesarepositorio = new despesarepositorio();
+            despesarepositorio.init();
+            mvx.registersingleton(despesarepositorio);
 
             RegisterAppStart<ViewModels.AdicionarDespesaViewModel>();
         }
--- GastoCerto/GastoCerto.Droid/Resources/Resource.Designer.cs
+++ GastoCerto/GastoCerto.Droid/Resources/Resource.Designer.cs
@@ -145,20 +145,23 @@ namespace GastoCerto.Droid
 			// aapt resource value: 0x7f050001
 			public const int MvxBindingTagUnique = 2131034113;
 			
-			// aapt resource value: 0x7f050005
-			public const int MyButton = 2131034117;
-			
-			// aapt resource value: 0x7f05000d
-			public const int btnBuscar = 2131034125;
-			
-			// aapt resource value: 0x7f05000c
-			public const int btnIncluir = 2131034124;
-			
-			// aapt resource value: 0x7f050004
-			public const int btnSalvar = 2131034116;
+			// aapt resource value: 0x7f050006
+			public const int MyButton = 2131034118;
 			
 			// aapt resource value: 0x7f05000e
-			public const int btnVoltar = 2131034126;
+			public const int btnBuscar = 2131034126;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnFoto = 2131034116;
+			
+			// aapt resource value: 0x7f05000d
+			public const int btnIncluir = 2131034125;
+			
+			// aapt resource value: 0x7f050005
+			public const int btnSalvar = 2131034117;
+			
+			// aapt resource value: 0x7f05000f
+			public const int btnVoltar = 2131034127;
 			
 			// aapt resource value: 0x7f050003
 			public const int editText1 = 2131034115;
@@ -166,23 +169,23 @@ namespace GastoCerto.Droid
 			// aapt resource value: 0x7f050002
 			public const int editText2 = 2131034114;
 			
-			// aapt resource value: 0x7f050007
-			public const int imageView1 = 2131034119;
-			
-			// aapt resource value: 0x7f05000a
-			public const int imageView2 = 2131034122;
-			
-			// aapt resource value: 0x7f050006
-			public const int linearLayout1 = 2131034118;
-			
-			// aapt resource value: 0x7f050009
-			public const int linearLayout2 = 2131034121;
-			
 			// aapt resource value: 0x7f050008
-			public const int textView1 = 2131034120;
+			public const int imageView1 = 2131034120;
 			
 			// aapt resource value: 0x7f05000b
-			public const int textView2 = 2131034123;
+			public const int imageView2 = 2131034123;
+			
+			// aapt resource value: 0x7f050007
+			public const int linearLayout1 = 2131034119;
+			
+			// aapt resource value: 0x7f05000a
+			public const int linearLayout2 = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView1 = 2131034121;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView2 = 2131034124;
 			
 			static Id()
 			{
